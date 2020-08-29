d460814ca4fb5f13e22a214d745f23bb549545e0
sagardas1 <sagardas.195@gmail.com>
2020-08-29 12:50:23 +0530

    changes

--- DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/RegistrationVo.java
+++ DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/RegistrationVo.java
@@ -8,36 +8,44 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "registration")
-public class RegistrationVo  {
+public class RegistrationVo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long uID;
 	private String userName;
 	private String password;
 	private boolean admin;
+
 	public long getuID() {
 		return uID;
 	}
+
 	public void setuID(long uID) {
 		this.uID = uID;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public boolean getAdmin() {
 		return admin;
 	}
+
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
-	
+
 }
