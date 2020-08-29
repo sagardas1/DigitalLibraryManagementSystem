3638e425637080d66c29c3d388d8113e3b7744bb
sagardas1 <sagardas.195@gmail.com>
2020-08-29 12:49:17 +0530

    changes

--- DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/Books.java
+++ DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/Books.java
@@ -8,36 +8,42 @@ import javax.persistence.Id;
 @Entity
 public class Books {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long BID;
 	private String bookName;
 	private Double price;
 	private String Genre;
+
 	public long getBID() {
 		return BID;
 	}
+
 	public void setBID(long bID) {
 		BID = bID;
 	}
+
 	public String getBookName() {
 		return bookName;
 	}
+
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getGenre() {
 		return Genre;
 	}
+
 	public void setGenre(String genre) {
 		Genre = genre;
 	}
-	
-	
 
 }
