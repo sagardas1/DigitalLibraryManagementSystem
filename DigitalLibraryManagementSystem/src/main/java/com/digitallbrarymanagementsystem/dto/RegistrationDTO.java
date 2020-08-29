9542aea4e98bd63a2fd64855c7dd06262e951478
sagardas1 <sagardas.195@gmail.com>
2020-08-29 12:48:51 +0530

    changes

--- DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/dto/RegistrationDTO.java
+++ DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/dto/RegistrationDTO.java
@@ -4,24 +4,29 @@ public class RegistrationDTO {
 	private String userName;
 	private String password;
 	private boolean admin;
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public boolean isAdmin() {
 		return admin;
 	}
+
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
-	
 
 }
