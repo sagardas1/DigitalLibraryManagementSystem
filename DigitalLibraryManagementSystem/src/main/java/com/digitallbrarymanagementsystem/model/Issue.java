ba35e24bd83da79c8b93106f53a04736a50ac3d6
sagardas1 <sagardas.195@gmail.com>
2020-08-29 12:49:28 +0530

    changes

--- DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/Issue.java
+++ DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/model/Issue.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class Issue {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long IID;
 	private long UID;
 	private long BID;
@@ -18,48 +18,61 @@ public class Issue {
 	private int period;
 	private LocalDate returnDate;
 	private double fine;
+
 	public long getIID() {
 		return IID;
 	}
+
 	public void setIID(long iID) {
 		IID = iID;
 	}
+
 	public long getUID() {
 		return UID;
 	}
+
 	public void setUID(long uID) {
 		UID = uID;
 	}
+
 	public long getBID() {
 		return BID;
 	}
+
 	public void setBID(long bID) {
 		BID = bID;
 	}
+
 	public LocalDate getIssueDate() {
 		return issueDate;
 	}
+
 	public void setIssueDate(LocalDate issueDate) {
 		this.issueDate = issueDate;
 	}
+
 	public int getPeriod() {
 		return period;
 	}
+
 	public void setPeriod(int period) {
 		this.period = period;
 	}
+
 	public LocalDate getReturnDate() {
 		return returnDate;
 	}
+
 	public void setReturnDate(LocalDate returnDate) {
 		this.returnDate = returnDate;
 	}
+
 	public double getFine() {
 		return fine;
 	}
+
 	public void setFine(double fine) {
 		this.fine = fine;
 	}
-	
 
 }
