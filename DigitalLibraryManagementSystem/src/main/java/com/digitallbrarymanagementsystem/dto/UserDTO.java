03a0077699fda58f4f5efe5a1a981cb1be12d978
sagardas1 <sagardas.195@gmail.com>
2020-08-29 12:49:04 +0530

    changes

--- DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/dto/UserDTO.java
+++ DigitalLibraryManagementSystem/src/main/java/com/digitallbrarymanagementsystem/dto/UserDTO.java
@@ -1,40 +1,39 @@
 package com.digitallbrarymanagementsystem.dto;
 
 public class UserDTO {
-	
+
 	private long uID;
 	private String userName;
 	private boolean isAdmin;
-	
-	
+
 	public UserDTO(long uID, String userName, boolean isAdmin) {
 		this.uID = uID;
 		this.userName = userName;
 		this.isAdmin = isAdmin;
 	}
+
 	public long getuID() {
 		return uID;
 	}
+
 	public void setuID(long uID) {
 		this.uID = uID;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public boolean isAdmin() {
 		return isAdmin;
 	}
+
 	public void setAdmin(boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
-	
-	
-	
-	
-	
-	
 
 }
